256b43d58fb1d3a21f12f34a1818f1d2ff2e9f86
Koji Takaki <pr5r1uqr@s.okayama-u.ac.jp>
2022-01-24 22:57:48 +0900

    some

--- plot3.R
+++ plot3.R
@@ -2,6 +2,6 @@ library(ggplot2)
 library(dplyr)
 
 png(filename = "plot3.png")
-g3 <- ggplot(baltimore_type, aes(year, Emissions, color = type)) + geom_line(show.legend = TRUE)
+g3 <- ggplot(baltimore_type, aes(year, Emissions, color = type)) + geom_line(aes(type), show.legend = TRUE)
 g3
 dev.off()
